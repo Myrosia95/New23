ccbd7a39f51afbae63ac505600e13a92159806d5
Miira95 <bozhovskamyrosla@gmail.com>
2023-10-01 22:52:08 +0200

    hw1.js

---
+++ pro-26-09/hw1/hw1.js
@@ -0,0 +1,25 @@
+// змінні із значенням та їх сума
+let a=5; 
+let b=7;
+let c=9;
+let d=2;
+
+let summ = a+b+c+d;
+console.log(`Сума всіх чисел = ${summ}`);
+console.log(`Сума всіх чисел = ${a+b+c+d}`);
+
+// середнє арифметичне
+// let midValue = (a+b+c+d)/4;
+let midValue = summ/4;
+
+console.log(`Середнє арифметичне = ${midValue}`);
+console.log(`Середнє арифметичне = ${(a+b+c+d) /4}`);
+
+// Створити змінну в якій розмістити розмір знижки будь-яке числове число, отримати від користувача ( за допомогою prompt )  кількість товарів і вартість товару,  вивести в консоль сумму товару - знижка.
+
+let discount = 30;
+let count = prompt('Кількість товару');
+let prise = prompt('Вартість товару');
+let totalPrise = Number(count)*Number(price);
+let discountPrice = totalPrise-(totalPrise*discount)/100;
+console.log(`сума до оплати ${discountPrice}`);
