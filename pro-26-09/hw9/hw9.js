82eda4f9f9677edbb8a97f6f68087577c7875295
Miira95 <bozhovskamyrosla@gmail.com>
2023-11-13 22:41:42 +0100

    hw9.js

---
+++ pro-26-09/hw9/hw9.js
@@ -0,0 +1,13 @@
+// Створити кнопку яке викликає модальне вікно, зробити копію компонента бутстрап
+window.onload = function () {
+    let modalWindow = document.querySelector('.modal');
+    let overlayBlock = document.querySelector('.overlay');
+
+    let buttons = document.querySelectorAll('.open, .close');
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            overlayBlock.classList.toggle('open');
+            modalWindow.classList.toggle('open');
+        });
+    });
+}
