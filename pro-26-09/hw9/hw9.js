8ef4bd7bfd6023296d9135eb59a4b832467e3458
Miira95 <bozhovskamyrosla@gmail.com>
2023-11-21 10:15:13 +0100

    hw9.js

--- pro-26-09/hw9/hw9.js
+++ pro-26-09/hw9/hw9.js
@@ -1,9 +1,11 @@
 // Створити кнопку яке викликає модальне вікно, зробити копію компонента бутстрап
+
 window.onload = function () {
     let modalWindow = document.querySelector('.modal');
     let overlayBlock = document.querySelector('.overlay');
 
-    let buttons = document.querySelectorAll('.open, .close');
+
+    let buttons = document.querySelector('.open', '.close');
     buttons.forEach(button => {
         button.addEventListener('click', function () {
             overlayBlock.classList.toggle('open');
@@ -11,3 +13,19 @@ window.onload = function () {
         });
     });
 }
+
+// Створити кнопку яке викликає модальне вікно, зробити копію компонента бутстрап
+// window.onload = function () {
+//     let modalWindow = document.querySelector('.modal');
+//     let overlayBlock = document.querySelector('.overlay');
+
+//     let buttons = document.querySelectorAll('.open, .close');
+//     buttons.forEach(button => {
+//         button.addEventListener('click', function () {
+//             overlayBlock.classList.toggle('open');
+//             modalWindow.classList.toggle('open');
+//         });
+//     });
+// }
+
+
